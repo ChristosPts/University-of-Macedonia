5a5a17e0120e500099b82a13ad96f1b1be96ddee
ChristosPts <105457831+ChristosPts@users.noreply.github.com>
2022-09-03 14:31:37 +0300

    Update a6f6.c

--- UOM-Procedural Programming/f6/a6f6.c
+++ UOM-Procedural Programming/f6/a6f6.c
@@ -1,19 +1,19 @@
 /*Write a program that displays averages of somatometric data of a group of N people
  (N is "constant" and is determined with the corresponding define command). N is equal to 5.
-  The program will perform the following operations using the following functions
+ The program will perform the following operations using the following functions
 
- >It will read N quadruplets of integers and insert them into a two-dimensional array
-  Each tetrad refers to the elements of a person, which are in order the following:
-  1)gender (0 if male, 1 if female)
-  2)the weight (in kilograms, integer value)
-  3)the height (in centimeters, integer value)
-  4)the age (in years, integer value)
+ > It will read N quadruplets of integers and insert them into a two-dimensional array
+   Each tetrad refers to the elements of a person, which are in order the following:
+     1)gender (0 if male, 1 if female)
+     2)the weight (in kilograms, integer value)
+     3)the height (in centimeters, integer value)
+     4)the age (in years, integer value)
 
-  >It will display the average weight, height and age for both males and females
-  Calculating the average for each element will be done with the help of one function void FindMean
-  In addition to the other parameters that you will define in the function, it will also have as a
-  parameter an index (values 1,2,3). The subscript will point to the corresponding element of the
-  quartet, whose average is calculated on the specific call, i.e. the appropriate column of the 2D array.
+  > It will display the average weight, height and age for both males and females
+    Calculating the average for each element will be done with the help of one function void FindMean
+    In addition to the other parameters that you will define in the function, it will also have as a
+    parameter an index (values 1,2,3). The subscript will point to the corresponding element of the
+    quartet, whose average is calculated on the specific call, i.e. the appropriate column of the 2D array.
 
   In the main() function the above functions/procedures will be called appropriately in order to
   read the data and then display the averages of weight, height and age of men and women. Averages
